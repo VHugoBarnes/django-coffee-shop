d8faf70729aabb4c3ff1e05b89ad3dd77a65ca42
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2024-09-15 21:13:54 -0500

    Add Product model to products/models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField()
+    available = models.BooleanField(default=True)
+    photo = models.ImageField(upload_to="products/photos", null=True, blank=True)
